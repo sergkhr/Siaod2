9e7239a94a31ab9d7a4b45a0862a57f2f43043b5
sergkhr <serg.khrechko@gmail.com>
2022-10-01 19:37:46 +0300

    made second second (no report though))

--- SecondTask/functions2.cpp
+++ SecondTask/functions2.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string>
 #include <vector>
-#include <bits/stdc++.h> 
+#include <cstring>
 
 using namespace std;
 struct Bank{
@@ -15,7 +15,7 @@ struct Bank{
 
 int toBinary(string fileName, string fileNameBin){//read from text file and write to binary file
 	ifstream fin(fileName);
-	fstream fout(fileNameBin, ios::in|ios::out|ios::binary);
+	fstream fout(fileNameBin, ios::out|ios::binary|ios::trunc);
 	if(!fin.is_open() || !fout.is_open()){
 		cout << "file doesn't exist" << endl;
 		fin.close();
@@ -26,11 +26,17 @@ int toBinary(string fileName, string fileNameBin){//read from text file and writ
 		Bank bank;
 		vector<string> tokens;
 		for (string each; getline(fin, each, ';'); tokens.push_back(each));
-		bank.id = stoi(tokens[0]);
-		strcpy(bank.name, tokens[1].c_str());
-		strcpy(bank.city, tokens[2].c_str());
-		bank.commercialOrState = (tokens[3] == "true") ? true : false;
-		fout.write((char*)&bank, sizeof(bank));
+		for(int i = 0; i < tokens.size(); i++){
+			//cout << tokens.size() << " " << tokens[i] << endl;
+			if(i%4 == 0) bank.id = stoi(tokens[i]);
+			else if(i%4 == 1) strcpy(bank.name, tokens[i].c_str());
+			else if(i%4 == 2) strcpy(bank.city, tokens[i].c_str());
+			else if(i%4 == 3){
+				bank.commercialOrState = (tokens[i] == "true") ? true : false;
+				fout.write((char*)&bank, sizeof(bank));
+			}
+		}
+		
 	}
 	fin.close();
 	fout.close();
@@ -47,10 +53,8 @@ int toText(string fileNameBin, string fileName){//read from binary file and writ
 		fout.close();
 		return 1;
 	}
-	
-	while(fin.good()){
-		Bank bank;
-		fin.read((char*)&bank, sizeof(bank));
+	Bank bank;
+	while(fin.read((char*)&bank, sizeof(bank))){
 		fout << bank.id << ";" << bank.name << ";" << bank.city << ";" << (bank.commercialOrState? "true" : "false") << endl;
 	}
 	fin.close();
@@ -65,28 +69,28 @@ int printBinary(string fileNameBin){//print from binary file
 		fin.close();
 		return 1;
 	}
-	while(fin.good()){
-		Bank bank;
-		fin.read((char*)&bank, sizeof(bank));
-		cout << bank.id << ";" << bank.name << ";" << bank.city << ";" << (bank.commercialOrState? "commercial" : "state") << endl;
+	Bank bank;
+	while(fin.read((char*)&bank, sizeof(bank))){
+		cout << bank.id << ";" << bank.name << ";" << bank.city << ";" << (bank.commercialOrState? "true" : "false") << endl;
 	}
 	fin.close();
 	return 0;
 }
 
-int getBank(string fileNameBin, Bank &bank, int id){//get bank by id from binary file
-	fstream fin(fileNameBin, ios::in|ios::out|ios::binary);
+int getBank(string fileNameBin, Bank &bank, int ind){//get bank by id from binary file
+	//returns 0 if bank was found, 1 if file doesn't exist, 2 if bank wasn't found
+	fstream fin(fileNameBin, ios::in|ios::binary);
 	if(!fin.is_open()){
 		cout << "file doesn't exist" << endl;
 		fin.close();
 		return 1;
 	}
 	
-	fin.seekg(sizeof(Bank)*(id-1), ios::beg);
+	fin.seekg(sizeof(Bank)*(ind-1), ios::beg);
 	Bank readBank; 
-	fin.read((char*)&bank, sizeof(bank));
+	fin.read((char*)&readBank, sizeof(Bank));
 	if(!fin.good()){
-		return false;
+		return 2;
 	}
 	bank = readBank;
 	fin.close();
@@ -94,6 +98,7 @@ int getBank(string fileNameBin, Bank &bank, int id){//get bank by id from binary
 }
 
 int deleteBank(string fileNameBin, int id){//delete bank by id from binary file
+	//returns 0 if bank was deleted, 1 if file doesn't exist, 2 if bank wasn't found
 	fstream fin(fileNameBin, ios::in|ios::out|ios::binary);
 	if(!fin.is_open()){
 		cout << "file doesn't exist" << endl;
@@ -101,41 +106,63 @@ int deleteBank(string fileNameBin, int id){//delete bank by id from binary file
 		return 1;
 	}
 	
-	fin.seekg(sizeof(Bank), ios::end);
-	Bank bank; 
-	fin.read((char*)&bank, sizeof(bank));//now bank contains the last record
-	fin.seekp(sizeof(Bank)*(id-1), ios::beg);
-	fin.write((char*)&bank, sizeof(bank));//teacher said that we have to open file in writing without appending mode to delete, will have to check
+	fin.seekg(-sizeof(Bank), ios::end);
+	Bank lastBank; 
+	fin.read((char*)&lastBank, sizeof(lastBank));
 
 	Bank bank;
 	fin.seekg(0, ios::beg);
-	do{
-		fin.read((char*)&bank, sizeof(bank));
+	while(fin.read((char*)&bank, sizeof(bank))){
+		cout << bank.id << endl;
 		if(bank.id == id){
-
+			fin.seekp(-sizeof(bank), ios::cur);
+			fin.write((char*)&lastBank, sizeof(lastBank));
+			fin.close();
+			return 0;
 		}
-	}while(fin.good());
+	}
+	fin.close();
+	return 2;
 }
 
-void changeBankCom(fstream &fin, int id){//change bank commercialOrState by id from binary file
-	if(fin.is_open()){
-		fin.seekg(sizeof(Bank)*(id-1), ios::beg);
-		Bank bank; 
+int changeOwnership(string fileNameBin, int id){//change bank commercialOrState by id from binary file
+	//returns 0 if bank was changed, 1 if file doesn't exist, 2 if bank wasn't found
+	fstream finoutBin(fileNameBin, ios::in|ios::out|ios::binary);
+	if(!finoutBin.is_open()){
+		cout << "file doesn't exist" << endl;
+		finoutBin.close();
+		return 1;
+	}
+
+	Bank bank;
+	finoutBin.seekg(0, ios::beg);
+	while(finoutBin.read((char*)&bank, sizeof(bank))){
+		if(bank.id == id){
+			bank.commercialOrState = !bank.commercialOrState;
+			finoutBin.seekp(-sizeof(bank), ios::cur);
+			finoutBin.write((char*)&bank, sizeof(bank));
+			finoutBin.close();
+			return 0;
+		}
+	}
+	finoutBin.close();
+	return 2;
+}
+
+int printBanksOfCity(string fileNameBin, string city){//print banks of city from binary file
+	fstream fin(fileNameBin, ios::in|ios::binary);
+	if(!fin.is_open()){
+		cout << "file doesn't exist" << endl;
+		fin.close();
+		return 1;
+	}
+	while(fin.good()){
+		Bank bank;
 		fin.read((char*)&bank, sizeof(bank));
-		bank.commercialOrState = !bank.commercialOrState;
-		fin.seekp(-sizeof(Bank), ios::cur);
-		fin.write((char*)&bank, sizeof(bank));
-	}
-}
-
-void printBanksOfCity(fstream &fin, string city){//print banks of city from binary file
-	if(fin.is_open()){
-		while(fin.good()){
-			Bank bank;
-			fin.read((char*)&bank, sizeof(bank));
-			if(bank.city == city && bank.commercialOrState){//banks of city and commercial
-				cout << bank.id << ";" << bank.name << ";" << bank.city << ";" << (bank.commercialOrState? "commercial" : "state") << endl;
-			}
+		if(bank.city == city && bank.commercialOrState){//banks of city and commercial
+			cout << bank.id << ";" << bank.name << ";" << bank.city << ";" << (bank.commercialOrState? "commercial" : "state") << endl;
 		}
 	}
+	fin.close();
+	return 0;
 }
--- SecondTask/secondTask2.cpp
+++ SecondTask/secondTask2.cpp
@@ -11,14 +11,19 @@ int main(){
 	string fileName;
 	cin >> fileName;
 	fileName += ".txt";
+	ifstream fin(fileName);
+	if(!fin.is_open()){
+		cout << "file doesn't exist" << endl;
+		return 1;
+	}
 
 
 	cout << "input file name for binary file (without .txt)" << endl;
 	string fileNameBin;
 	cin >> fileNameBin;
 	fileNameBin += ".txt";
-	fstream finoutBin(fileNameBin, ios::in|ios::out|ios::binary);
-	if(!finoutBin.is_open()){
+	fstream finBin(fileNameBin, ios::in|ios::binary);
+	if(!finBin.is_open()){
 		cout << "file doesn't exist" << endl;
 		return 1;
 	}
@@ -35,23 +40,44 @@ int main(){
 
 	toBinary(fileName, fileNameBin);
 	toText(fileNameBin, fileNameText);
+	cout << "\ncontents of binary file:" << endl;
 	printBinary(fileNameBin);
-	cout << "input id of bank" << endl;
-	int id;
-	cin >> id;
+
+	cout << "\ninput number of bank (starts from 1)" << endl;
+	int num;
+	cin >> num;
 	Bank bank;
-	if(getBank(finoutBin, bank, id)){
+	if(getBank(fileNameBin, bank, num) == 0){
 		cout << "id: " << bank.id << endl;
 		cout << "name: " << bank.name << endl;
 		cout << "city: " << bank.city << endl;
 		cout << (bank.commercialOrState? "commercial" : "state") << endl;
 	}
 	else cout << "no such bank" << endl;
-	
+
+	cout << "\ninput id of bank you want to delete" << endl;
+	int id;
+	cin >> id;
+	if(deleteBank(fileNameBin, id) == 0) cout << "bank deleted" << endl;
+	else cout << "no such bank" << endl;
+	cout << "\ncontents of binary file:" << endl;
+	printBinary(fileNameBin);
+
+	cout << "\ninput the id of the bank that you want to cahnge ownership of" << endl;
+	cin >> id;
+	if(changeOwnership(fileNameBin, id) == 0) cout << "ownership changed" << endl;
+	else cout << "no such bank" << endl;
+	cout << "\ncontents of binary file:" << endl;
+	printBinary(fileNameBin);
+
+	cout << "\ninput name of the city banks of which you want to print" << endl;
+	string city;
+	cin >> city;
+	printBanksOfCity(fileNameBin ,city);
 
 
 	fin.close();
-	finoutBin.close();
+	finBin.close();
 	foutText.close();
 	return 0;
 }
